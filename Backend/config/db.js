1d6be18769cde6a27cadbd20c56f9d103d0a163d
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-29 11:51:16 -0500

    added mongo in memory server and env variables

---
+++ Backend/config/db.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server"
+
+let mongoServer;
+
+export const connectDB = async () => {
+    //Avoid depreceation noise and make queries fast 
+    mongoose.set("strictQuery", true);
+
+    const uri = process.env.MONGO_URI;
+
+    const useMemoryDB = process.env.USE_MEMORY_DB === "true" || !uri;
+
+    if(useMemoryDB){
+        mongoServer = await MongoMemoryServer.create();
+        uri = mongoServer.getUri();
+        console.log("Using in memory server")
+    }
+
+    if(!uri){
+        throw new Error("MONGOURI not defined in env")
+    }
+
+    const conn = await mongoose.connect(uri, {
+        serverSelectionTimeoutMS: 10000,
+    });
+
+    console.log(`Mongo db connected ${conn.connection.host}/${conn.connection.name}` );
+    return conn 
+}
+
+export const stopDB = async () => {
+    await mongoose.disconnect();
+
+    if(mongoServer){
+        await mongoServer.stop();
+        console.log("In memory server stopped")
+    }
+}
