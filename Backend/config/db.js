09f41dbadd2f1ccc5bbbdfc1ebc6a175783f3254
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-01 11:31:46 -0500

    deleted mongo memory serve from db.js to use mongo community server

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,24 +1,14 @@
 import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server"
-
-let mongoServer;
 
 export const connectDB = async () => {
-    //Avoid depreceation noise and make queries fast 
+    try {
+        //Avoid depreceation noise and make queries fast 
     mongoose.set("strictQuery", true);
 
     let uri = process.env.MONGO_URI;
 
-    const useMemoryDB = process.env.USE_MEMORY_DB === "true" || !uri;
-
-    if(useMemoryDB){
-        mongoServer = await MongoMemoryServer.create();
-        uri = mongoServer.getUri();
-        console.log("Using in memory server")
-    }
-
     if(!uri){
-        throw new Error("MONGOURI not defined in env")
+        throw new Error("MONGO_URI not defined in env")
     }
 
     const conn = await mongoose.connect(uri, {
@@ -27,13 +17,13 @@ export const connectDB = async () => {
 
     console.log(`Mongo db connected ${conn.connection.host}/${conn.connection.name}` );
     return conn 
-}
+    } catch (error) {
+        console.error("MongoDB Connection Failed", error.message)
+    }
+ 
+};  
 
 export const stopDB = async () => {
     await mongoose.disconnect();
-
-    if(mongoServer){
-        await mongoServer.stop();
-        console.log("In memory server stopped")
-    }
-}
+    console.log("MongoDB disconnected");
+};
